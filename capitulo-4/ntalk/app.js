aaaa5b0aa3c58685a8e0d3f3c9a780268a03d032
Carlos Roberto <crpu4a@gmail.com>
2021-04-19 12:55:13 -0300

    Configuração inicial do Servidor.

--- capitulo-4/ntalk/app.js
+++ capitulo-4/ntalk/app.js
@@ -1,41 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const routes = require('./routes/index')
+const users = require('./routes/users')
+const app = express()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Configurações Internas
+app.set('views', path.join(__dirname, 'views')) // Set views.
+app.set('view engine', 'ejs')
 
-var app = express();
+// Middlewares.
+app.use( express.static( path.join(__dirname, 'public') ) ) // Set arquivos estáticos
+app.use('/', routes) // Rotas
+app.use('/usuarios', users)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3000, console.log('Ntalk no ar. http://localhost:3000'))
