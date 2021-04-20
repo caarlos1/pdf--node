8cc4f827bdda0f0839018291afeefeb8e81ef037
Carlos Roberto <crpu4a@gmail.com>
2021-04-20 12:19:12 -0300

    Adição de middlewares de erro e autenticador.

--- capitulo-4/ntalk/app.js
+++ capitulo-4/ntalk/app.js
@@ -4,6 +4,8 @@ const consign = require('consign') // Para importar modulos.
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
+const methodOverride = require('method-override')
+const error = require('./middlewares/error')
 const app = express()
 
 // Configurações Internas
@@ -15,6 +17,7 @@ app.use( cookieParser('ntalk') )
 app.use( expressSession() )
 app.use( bodyParser.json() )
 app.use( bodyParser.urlencoded() )
+app.use( methodOverride('_method') ) // Para conseguir enviar um put e delete pelo formulario, sem ajax.
 app.use( express.static( path.join(__dirname, 'public') ) ) // Set arquivos estáticos
 
 
@@ -24,5 +27,9 @@ consign({})
   .then('controllers')
   .then('routes')
   .into(app)
+
+// Middlewares de tratamento de erros
+app.use(error.notFound)
+app.use(error.serverError)
 
 app.listen(3000, console.log('Ntalk no ar. http://localhost:3000'))
