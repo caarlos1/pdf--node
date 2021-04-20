0e9179bc619a4753e4956e4833df9207f6766cb7
Carlos Roberto <crpu4a@gmail.com>
2021-04-20 10:07:58 -0300

    Criação de rodas da pagina contatos e sessão.

--- capitulo-4/ntalk/app.js
+++ capitulo-4/ntalk/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const consign = require('consign') // Para importar modulos.
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
 const app = express()
 
 // Configurações Internas
@@ -8,7 +11,12 @@ app.set('views', path.join(__dirname, 'views')) // Set views.
 app.set('view engine', 'ejs')
 
 // Middlewares.
+app.use( cookieParser('ntalk') )
+app.use( expressSession() )
+app.use( bodyParser.json() )
+app.use( bodyParser.urlencoded() )
 app.use( express.static( path.join(__dirname, 'public') ) ) // Set arquivos estáticos
+
 
 // Carrega sozinho os modulos nas seguintes diretórios:
 consign({})
