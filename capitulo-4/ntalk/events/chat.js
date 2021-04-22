14bf2d9bb78f79324798bcf19f178529cf97ac7d
Carlos Roberto <crpu4a@gmail.com>
2021-04-22 12:40:34 -0300

    Notificadores na Agenda de Contatos.

--- capitulo-4/ntalk/events/chat.js
+++ capitulo-4/ntalk/events/chat.js
@@ -1,8 +1,16 @@
 module.exports = (app, io) => {
 // Conexão do Socket.io
+    const onlines = {} // Usuários online.
     io.on('connection', (client) => {
         const { session } = client.handshake
         const { usuario } = session
+
+        // Crio uma chave e valor no objeto com email e email.
+        onlines[usuario.email] = usuario.email
+        for(let email in onlines){
+            client.emit('notify-onlines', email)
+            client.broadcast.emit('notify-onlines', email)
+        }
         
         // Quando o cliente mandar mensagem para o servidor:
         client.on('send-server', (hashDaSala, msg) => {
@@ -21,11 +29,17 @@ module.exports = (app, io) => {
             client.join(hashDaSala) // Inclui a conexão do usuário na sala.
         } )
 
-        // Desconectando da sala.
+        // Executa quando o cliente é desconectádo por algum motivo.
         client.on('disconnect', () => {
             const { sala } = session
+            const resposta = `<b>${ usuario.nome }</b> saiu da sala.`
+            delete onlines[usuario.email] // Excluo da lista de online o usuario que desconectou.
+            
             session.sala = null
-            client.leave(sala)
+            client.leave(sala) // Tira a conexão do usuário da sala.
+
+            client.broadcast.emit('notify-offlines', usuario.email) // Notifica que o usuario está offline.
+            io.to(sala).emit('send-client', resposta) // Mnada para sala a mensagem de disconnect.
         })
 
     })
