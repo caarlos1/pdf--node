79c89e810c0e62c67d6c898a85127a522f4df668
Carlos Roberto <crpu4a@gmail.com>
2021-04-22 10:45:14 -0300

    Adição de chat com sessões privadas.

--- capitulo-4/ntalk/controllers/chat.js
+++ capitulo-4/ntalk/controllers/chat.js
@@ -1,7 +1,19 @@
+const crypto = require('crypto')
+
 module.exports = (app) => {
     const ChatController = {
         index (req, res) {
-            res.render('chat/index')
+            const { sala } = req.query // Pego a query ?sala= da url para salvar o hash da conversa.
+            let hashDaSala = sala
+
+            // Caso não exista hash, é criada uma.
+            if(!hashDaSala){
+                const timestamp = Date.now().toString()
+                const md5 = crypto.createHash('md5')
+                hashDaSala = md5.update(timestamp).digest('hex')
+            }
+
+            res.render('chat/index', { sala: hashDaSala }) // O hash será o nome da sala.
         }
     }
     return ChatController
--- capitulo-4/ntalk/events/chat.js
+++ capitulo-4/ntalk/events/chat.js
@@ -5,10 +5,28 @@ module.exports = (app, io) => {
         const { usuario } = session
         
         // Quando o cliente mandar mensagem para o servidor:
-        client.on('send-server', (msg) => {
+        client.on('send-server', (hashDaSala, msg) => {
+            const novaMensagem = { email: usuario.email, sala: hashDaSala }
             const resposta = `<b> ${usuario.nome}:</b> ${ msg } </br>`
-            client.emit('send-client', resposta) // Eviaa a resposta para o usuario.
-            client.broadcast.emit('send-client', resposta) // Envia a respostas para todos usuarios menos o emissor.
+            
+            session.sala = hashDaSala
+
+            client.broadcast.emit('new-message', novaMensagem) // Avisar que uma mensagem foi enviada paro o usulário.
+            io.to(hashDaSala).emit('send-client', resposta) // Envia a mensagem para a sala com o hash
         } )
+
+        // Evento escuta e cria as salas com o hash recebido.
+        client.on('create-room', hashDaSala => {
+            session.sala = hashDaSala // Adiciona a sala no usuário
+            client.join(hashDaSala) // Inclui a conexão do usuário na sala.
+        } )
+
+        // Desconectando da sala.
+        client.on('disconnect', () => {
+            const { sala } = session
+            session.sala = null
+            client.leave(sala)
+        })
+
     })
 }
