d6127598f00d18b9b8720be812fc7ace2f1b6100
Carlos Roberto <crpu4a@gmail.com>
2021-04-28 11:33:06 -0300

    Arquivos de testes do mocha.

---
+++ capitulo-4/ntalk/test/requests/contatos.js
@@ -0,0 +1,93 @@
+const app = require('../../app')
+const request = require('supertest')(app)
+
+describe( 'Controller contatos', () => {
+    describe( 'Usuario não logado', () => {
+        
+        it( 'GET "/contatos" redireciona para GET "/"' , done => {
+            request.get('/contatos')
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/')
+                    done()
+                } )
+        })
+
+        it( 'GET "/contato/1" redireciona para GET "/"' , done => {
+            request.get('/contato/1')
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/')
+                    done()
+                } )
+        })
+
+        it( 'GET "/contato/1/editar" redireciona para GET "/"' , done => {
+            request.get('/contato/1/editar')
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/')
+                    done()
+                } )
+        })
+
+        it( 'POST "/contato" redireciona para GET "/"' , done => {
+            request.post('/contato')
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/')
+                    done()
+                } )
+        })
+
+        it( 'DELETE "/contato/1" redireciona para GET "/"' , done => {
+            request.del('/contato/1')
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/')
+                    done()
+                } )
+        })
+
+        it( 'PUT "/contato/1" redireciona para GET "/"' , done => {
+            request.put('/contato/1')
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/')
+                    done()
+                } )
+        })
+
+    } )
+
+    describe( 'Usuario logado', () => {
+        const usuario = { nome: 'Teste', email: 'teste@teste' }
+        let cookie = null
+
+        beforeEach( done => {
+            request.post( '/entrar' )
+                .send( { usuario } )
+                .end( (err, res) => {
+                    cookie = res.headers['set-cookie']
+                    done()
+                } )
+        } )
+
+        it( 'GET "/contatos" retorna status 200', done => {
+            const req = request.get('/contatos')
+            req.cookies = cookie // Setamos o cookie da requisição
+
+            req.end( (err, res) => {
+                res.status.should.eql(200)
+                done()
+            } )
+
+        } )
+
+        it( 'POST "/contato" redireciona para GET "/contatos"', done => {
+            const contato = usuario
+            const req = request.post('/contato')
+            req.cookies = cookie // Setamos o cookie da requisição
+            req.send( { contato } )
+                .end( (err, res) => {
+                    res.headers.location.should.eql('/contatos')
+                    done()
+                } )
+        } )
+
+    } )
+} )
